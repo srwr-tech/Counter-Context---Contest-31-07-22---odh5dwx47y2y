84aedb44e57f9676b577b3029bedcc40830070ae
Sarwar Alam <100376398+srwr-tech@users.noreply.github.com>
2022-07-31 19:20:46 +0530

    Update counterContext.js

--- src/counterContext.js
+++ src/counterContext.js
@@ -8,7 +8,7 @@ const counterContext = createContext()
 
 
 const CounterStateContext = (props) => {
-
+    const[count,setCount]=useState(0);
     return (
         <div id="counter-context">
             <counterContext.Provider value={{ count, setCount }}>
